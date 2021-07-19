01cc57aa64e6ad04b29c9612983df49e4c5df6eb
KabohaJeanMark <kabohajeanmark@gmail.com>
2021-07-19 17:26:52 +0300

    Add a group model with the association belongs_to user. Update user model with association has_many groups

---
+++ db/migrate/20210719142352_create_groups.rb
@@ -0,0 +1,10 @@
+class CreateGroups < ActiveRecord::Migration[6.1]
+  def change
+    create_table :groups do |t|
+      t.string :name
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
