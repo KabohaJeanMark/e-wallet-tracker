fbd3b424826770b176d2599b088dfa14b953e10f
KabohaJeanMark <kabohajeanmark@gmail.com>
2021-07-20 13:23:43 +0300

    Add new and create actions for GroupsController with strong params

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -6,4 +6,24 @@ class GroupsController < ApplicationController
   def show
     @group = Group.find(params[:id])
   end
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    @group = current_user.groups.build(group_params)
+
+    if @group.save
+      redirect_to root_url
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:name)
+  end
 end
