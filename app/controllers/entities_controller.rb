0e42a9d782f42089bd8dbe62104168ac24e35ef6
KabohaJeanMark <kabohajeanmark@gmail.com>
2021-07-20 21:57:56 +0300

    Refactor and add a partial for cleaner code

--- app/controllers/entities_controller.rb
+++ app/controllers/entities_controller.rb
@@ -6,26 +6,28 @@ class EntitiesController < ApplicationController
   end
 
   def new
-    @group_options = [['none', nil]]
-    @group_options += Group.all.map { |g| [g.name, g.id] }
+    @groups = Group.all.to_a
     @entity = Entity.new
   end
 
   def create
     @entity = current_user.entities.build(entity_params)
+
     if @entity.save
-      redirect_to root_url
+      redirect_to root_url, notice: 'Transaction was successfully created.'
     else
-      render :new
+      render :new, alert: 'Transaction failed to create'
     end
   end
 
   def my_entities
     @my_entities = current_user.entities.where.not(group_id: nil)
+    @total_amount = @my_entities.sum(:amount)
   end
 
   def my_external_entities
     @my_ext_entities = current_user.entities.where(group_id: nil)
+    @total_ext_amount = @my_ext_entities.sum(:amount)
   end
 
   private
--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -17,9 +17,9 @@ class GroupsController < ApplicationController
     @group = current_user.groups.build(group_params)
 
     if @group.save
-      redirect_to groups_path
+      redirect_to groups_path, notice: 'Group was successfully created.'
     else
-      render :new
+      render :new, alert: 'Group failed to create'
     end
   end
 
